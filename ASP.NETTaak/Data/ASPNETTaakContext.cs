2363d795df514ca87f078e0af3d204e26831326c
LoesK-immalle <loes.kenis@immalle.be>
2018-03-11 19:26:48 +0100

    Step 4

---
+++ ASP.NETTaak/Data/ASPNETTaakContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NETTaak.Models
+{
+    public class ASPNETTaakContext : DbContext
+    {
+        public ASPNETTaakContext (DbContextOptions<ASPNETTaakContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP.NETTaak.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ ASP.NETTaak/Migrations/20180311182311_Initial.Designer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ASP.NETTaak.Models;
+
+namespace ASP.NETTaak.Migrations
+{
+    [DbContext(typeof(ASPNETTaakContext))]
+    [Migration("20180311182311_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.5")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ASP.NETTaak.Models.Movie", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Genre");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<DateTime>("ReleaseDate");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Movie");
+                });
+        }
+    }
+}
